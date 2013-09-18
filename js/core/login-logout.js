8fbe3806e3803f6992082159e7181bac390a9eb4
Susan Brown <brownsusan@fullsail.edu>
2013-09-18 19:29:40 -0400

    commit

---
+++ js/core/login-logout.js
@@ -0,0 +1,25 @@
+$.ajax({
+	url: 'xhr/login.php',
+	data: {
+		username: user,
+		password: pass
+	},
+	type: 'post',
+	dataType: 'json',
+	success: function(response){
+		if(response.error){
+			showLoginError();
+		}else{
+			loadApp();
+		};
+	}
+	return false;
+});
+
+win.on('click', '#btn-logout', function(){
+	$.get('xhr/logout.php', function(){
+		loadLanding();
+	})
+	return false;
+})
+
--- js/core/main.js
+++ js/core/main.js
@@ -1,4 +1,47 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
+	//Check login
+    var init = function () {
+        $.ajax({
+            url: "xhr/check_login.php",
+            type: "get",
+            dataType: "json",
+            success: function (response) {
+                console.log(response);
+                
+                // if the user is logged in
+                if (response.user) {
+                   loadApplication();
+                } 
+                
+                // if the user is not logged in
+                else {
+                	loadLanding();
+                }
+            } 
+        }); 
+    }; 
+    
+    init();
+    
+    function loadLanding(){
+	    
+	    console.log("The landing page will be loaded here");
+	    
+	    // creat all the page elements
+	    
+	    // add the events to the elements
+	    
+	    $('#someID').click(function(){
+		    
+	    });
+	    
+    }
+    
+    function loadApplication(){
+	    
+	    console.log("The application will be loaded here");
+	    
+    }
+    
 });
-
