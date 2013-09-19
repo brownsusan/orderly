3a207aedfc5f0619fd1f9bad70f2162c8b86a26e
Susan Brown <ajames@Landmine.fullsail.com>
2013-09-18 20:00:36 -0400

    adds a login function.

--- js/core/main.js
+++ js/core/main.js
@@ -1,47 +1,88 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-	//Check login
-    var init = function () {
-        $.ajax({
-            url: "xhr/check_login.php",
-            type: "get",
-            dataType: "json",
-            success: function (response) {
-                console.log(response);
-                
-                // if the user is logged in
-                if (response.user) {
-                   loadApplication();
-                } 
-                
-                // if the user is not logged in
-                else {
-                	loadLanding();
-                }
-            } 
-        }); 
-    }; 
-    
-    init();
-    
-    function loadLanding(){
-	    
-	    console.log("The landing page will be loaded here");
-	    
-	    // creat all the page elements
-	    
-	    // add the events to the elements
-	    
-	    $('#someID').click(function(){
-		    
-	    });
-	    
-    }
-    
-    function loadApplication(){
-	    
-	    console.log("The application will be loaded here");
-	    
-    }
-    
+	var init = function() {
+
+		// check if the user is logged in
+		$.ajax({
+			url : "xhr/check_login.php",
+			type : "get",
+			dataType : "json",
+			success : function(response) {
+
+				console.log(response);
+
+				// if the user is logged in
+				if (response.user) {
+					loadApplication();
+				}
+
+				// if the user is not logged in
+				else {
+					loadLanding();
+				}
+			}
+		});
+
+	};
+
+	init();
+
+	// create the landing page
+	function loadLanding() {
+
+		console.log("The landing page will be loaded here");
+
+		// creat all the page elements
+
+		// add the events to the elements
+
+		$('#login_submit').click(function() {
+
+			var username = $('#login_username').val();
+			var password = $('#login_password').val();
+
+			// validate the variables
+
+			var valid = true;
+
+			if (username.length == 0) {
+				valid = false;
+				// show the user an error
+			}
+
+			if (password.length < 6) {
+				valid = false;
+				// show the user an error
+			}
+
+			if (!valid) {
+				return;
+			}
+
+			// log the user in
+
+			$.ajax({
+				url : "xhr/login.php",
+				type : "post",
+				dataType : "json",
+				data : {
+					'username' : username,
+					'password' : password
+				},
+				success : function(response) {
+					console.log(response);
+				}
+			});
+
+		});
+
+	}
+
+	// create the application page
+	function loadApplication() {
+
+		console.log("The application will be loaded here");
+
+	}
+
 });
