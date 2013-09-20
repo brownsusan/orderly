7fcefcf07f8bb04bd0be30c2e36ccd4a3ce5358b
Susan Brown <brownsusan@fullsail.edu>
2013-09-20 03:34:24 -0400

    templating

--- js/core/main.js
+++ js/core/main.js
@@ -35,47 +35,90 @@ $(document).ready(function() {
 		console.log("The landing page will be loaded here");
 
 		// create all the page elements
-
-		// add the event listener to the elements
-
-		$('#login_submit').click(function() {
-
-			var username = $('#login_username').val();
-			var password = $('#login_password').val();
-
-			// validate the variables
-
-			var valid = true;
-
-			if (username.length == 0) {
-				valid = false;
-				// show the user an error
-			}
-
-			if (password.length < 6) {
-				valid = false;
-				// show the user an error
-			}
-
-			if (!valid) {
-				return;
-			}
-
-			// log the user in
-
-			$.ajax({
-				url : "xhr/login.php",
-				type : "post",
-				dataType : "json",
-				data : {
-					'username' : username,
-					'password' : password
-				},
-				success : function(response) {
-					console.log(response);
+		$.get("templates/landing.html", function(template){
+			var header = $(template).find('#landing-header').html();
+			var main = $(template).find('#landing-main').html();
+			
+			/*
+			Do not need clears because .html() replaces the html
+			If I were using .append() I would need to clear it first then append the html to the header/main
+			// clear the header
+			$('header').html('');
+			//clear the main
+			$('#main').html('');
+			*/
+			
+			//replace the header
+			$('header').html(header);
+			
+			//replace the main
+			$('#main').html(main);
+			
+			// add the event listener to the elements
+	
+			$('#header_login').click(function() {
+	
+				var username = $('#header_username').val();
+				var password = $('#header_password').val();
+	
+				// validate the variables
+	
+				var valid = true;
+	
+				if (username.length == 0) {
+					valid = false;
+					// show the user an error
 				}
+	
+				if (password.length < 6) {
+					valid = false;
+					// show the user an error
+				}
+	
+				if (!valid) {
+					return;
+				}
+	
+				// log the user in
+	
+				$.ajax({
+					url : "xhr/login.php",
+					type : "post",
+					dataType : "json",
+					data : {
+						'username' : username,
+						'password' : password
+					},
+					success : function(response) {
+					
+						// if the user logged in
+						if (response.user) {
+							loadApplication();
+						}
+		
+						// if the user did not log in
+						else {
+							// show error
+						}
+	
+					}
+				});
+	
 			});
-
+			
+			$('#home_sign_up').click(function() {
+				
+				// make the variables
+				var firstName = $('#home_firstName').val();
+				
+				// validate the variable
+				
+				// ajax request to send the variables
+				
+				// use login as an example
+				
+			});
+			
 		});
 
 	}
@@ -87,24 +130,82 @@ $(document).ready(function() {
 		console.log("The application will be loaded here");
 
 		// create all the page elements
-
-		// add the event listener to the elements
-
-		$('#logout_submit').click(function() {
-
-			// log the user out
-
-			$.ajax({
-				url : "xhr/logout.php",
-				type : "get",
-				dataType : "json",
-				success : function(response) {
-					console.log(response);
-				}
+		//?2 is for forcing it to reload - remove before submitting - clear cache to remove the need to reload
+		//HACK
+		$.get("templates/app.html?2", function(template){
+			
+			var header = $(template).find('#application-header').html();
+			
+			//replace the header
+			$('header').html(header);
+			
+			loadProjects();
+			
+			// add the event listener to the elements
+	
+			$('#header_logout').click(function() {
+	
+				// log the user out
+	
+				$.ajax({
+					url : "xhr/logout.php",
+					type : "get",
+					dataType : "json",
+					success : function(response) {
+						
+						// if the user logged out
+						if (response.success) {
+							loadLanding();
+						}
+		
+						// if the user did not log out
+						else {
+							// show error
+						}
+						
+					}
+				});
+	
 			});
-
+			
 		});
+		
 
+
+	}
+	
+	
+	function loadProjects(){
+	
+		// clear the main
+		$('#main').html('');
+	
+		$.ajax({
+			url : "xhr/get_projects.php",
+			type : "get",
+			dataType : "json",
+			success : function(response) {
+				console.log(response);
+			}
+		});
+	}
+	
+	function loadTasks(projectID){
+	
+		// clear the main
+		$('#main').html('');
+	
+		$.ajax({
+			url : "xhr/get_tasks.php",
+			type : "get",
+			dataType : "json",
+			data: {
+				'projectID': projectID
+			},
+			success : function(response) {
+				console.log(response);
+			}
+		});
 	}
 
 });
