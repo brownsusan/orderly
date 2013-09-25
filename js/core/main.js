9a9f5608dc997797cf981d0ada4adfa8fe1b9574
Alan James <ajames@Landmine.(none)>
2013-09-25 03:54:15 -0400

    Commit

--- js/core/main.js
+++ js/core/main.js
@@ -196,7 +196,7 @@ function loadApplication() {
 	// create all the page elements
 	//?2 is for forcing it to reload - remove before submitting - clear cache to remove the need to reload
 	//HACK
-	$.get("templates/app.html?5", function(template) {
+	$.get("templates/app.html?6", function(template) {
 		applicationTemplate = template;
 
 		var header = $(applicationTemplate).find('#application-header').html();
